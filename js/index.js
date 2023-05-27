1d171dbd0180257ff1e077118b1d61bc49dc74c9
MonashBishal <134600787+MonashBishal@users.noreply.github.com>
2023-05-28 04:27:51 +1000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+/* https://www.w3schools.com/howto/howto_js_dropdown.asp */
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+ 
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  } 
+}
+
+
+  function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+   
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn1')) {
+    var myDropdown = document.getElementById("myDropdown1");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
